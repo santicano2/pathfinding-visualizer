d046772f30635aeaf9d5cd9c0e546312b8e87a7e
Santi <canosantiago404@gmail.com>
2024-07-06 16:25:43 -0300

    añadido context y provider de pathfinding

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { PathfindingProvider } from "./context/PathfindingContext";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">
-      Hello world!
-    </h1>
+    <PathfindingProvider>
+      <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">
+        Hello world!
+      </h1>
+    </PathfindingProvider>
   );
 }
 
