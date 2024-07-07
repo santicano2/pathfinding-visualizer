07e1919fecdd2909b7720c05c3374441d2f954f8
Santi <canosantiago404@gmail.com>
2024-07-07 10:45:59 -0300

    agregado speedContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { PathfindingProvider } from "./context/PathfindingContext";
+import { SpeedProvider } from "./context/SpeedContext";
 import { TileProvider } from "./context/TileContext";
 
 function App() {
   return (
     <PathfindingProvider>
       <TileProvider>
-        <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">
-          Hello world!
-        </h1>
+        <SpeedProvider>
+          <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">
+            Hello world!
+          </h1>
+        </SpeedProvider>
       </TileProvider>
     </PathfindingProvider>
   );
